d79f9a575a171a957749a3a1c91850698cc31a86
Boopslo <oslophantom@gmail.com>
2015-08-03 22:49:37 -0400

    CompoundView first commit

---
+++ app/src/main/java/practice/compoundviewexample/MainActivity.java
@@ -0,0 +1,32 @@
+package practice.compoundviewexample;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void onClickFunction(View view){
+        String text = (view.getId() == R.id.view1) ? "Background" : "Foreground";
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+
+}
